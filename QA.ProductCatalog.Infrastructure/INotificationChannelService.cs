3079cd8422c172fc32799fb5b1fcbca0d9e2f525
Sergey Koryakin <KoryakinS@quantumart.ru>
2016-10-11 12:04:39 +0300

    initial commit #77763

---
+++ QA.ProductCatalog.Infrastructure/INotificationChannelService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA.ProductCatalog.Infrastructure
+{
+    public interface INotificationChannelService
+    {
+        NotificationChannel[] GetNotificationChannels();
+        void UpdateNotificationChannel(string name, int productId, string publicationStatus);
+    }
+}
---
+++ QA.ProductCatalog.Infrastructure/NotificationSenderChannel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA.ProductCatalog.Infrastructure
+{
+    public class NotificationSenderChannel
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public string LastStatus { get; set; }
+        public DateTime? LastPublication { get; set; }
+        public int? LastId { get; set; }
+    }
+}
