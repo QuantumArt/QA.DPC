4ae54c2d282c75e1bba4abef5322d1c955769681
Sergey Koryakin <KoryakinS@quantumart.ru>
2016-12-02 19:32:21 +0300

    add HighloadFront custom action #82151

---
+++ QA.ProductCatalog.Admin.WebApp/Controllers/HighloadFrontController.cs
@@ -0,0 +1,17 @@
+﻿using QA.Core.Web;
+using System.Web.Mvc;
+
+namespace QA.ProductCatalog.Admin.WebApp.Controllers
+{
+    [RequireCustomAction]
+	public class HighloadFrontController : Controller
+	{
+		public HighloadFrontController()
+		{
+		}
+		public ActionResult Index(string url)
+		{
+            return View(url);
+		}	
+	}
+}
