8145fecd39d5a39f599aea6029daba2367235458
Sergey Koryakin <KoryakinS@quantumart.ru>
2017-04-25 17:35:57 +0300

    process unaccessible customer codes

--- QA.Core.DPC.QP/Servives/CustomerProvider.cs
+++ QA.Core.DPC.QP/Servives/CustomerProvider.cs
@@ -3,16 +3,19 @@ using QA.Core.DPC.QP.Models;
 using Quantumart.QP8.Configuration;
 using Quantumart.QPublishing.Database;
 using System.Data.SqlClient;
+using System;
 
 namespace QA.Core.DPC.QP.Servives
 {
     public class CustomerProvider : ICustomerProvider
     {
         private readonly QPConfiguration _configuration;
+        private readonly ILogger _logger;
 
-        public CustomerProvider()
+        public CustomerProvider(ILogger logger)
         {
             _configuration = new QPConfiguration();
+            _logger = logger;
         }
 
         public string GetConnectionString(string customerCode)
@@ -28,15 +31,23 @@ namespace QA.Core.DPC.QP.Servives
                     ConnecdtionString = QPConfiguration.GetConnectionString(c),
                     CustomerCode = c
                 })
-                .Where(itm => IsDPCMode(itm.ConnecdtionString))
+                .Where(customer => IsDPCMode(customer))
                 .ToArray();
         }
 
-        public bool IsDPCMode(string sonnectionString)
+        public bool IsDPCMode(Customer customer)
         {
-            var connector = new DBConnector(sonnectionString);
-            var command = new SqlCommand("SELECT USE_DPC FROM DB");
-            return (bool)connector.GetRealScalarData(command);
+            try
+            {
+                var connector = new DBConnector(customer.ConnecdtionString);
+                var command = new SqlCommand("SELECT USE_DPC FROM DB");
+                return (bool)connector.GetRealScalarData(command);
+            }
+            catch(Exception)
+            {
+                _logger.LogError(() => $"Customer code {customer.CustomerCode} is not accessible");
+                return false;
+            }
         }
     }
 }
