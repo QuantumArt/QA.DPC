276e38a08a632f130d7e36bc5fb6fbae061a7bfc
Sergey Koryakin <KoryakinS@quantumart.ru>
2017-05-31 19:37:07 +0300

    update webApi

--- QA.Core.DPC.API/Container/APIContainerExstensions.cs
+++ QA.Core.DPC.API/Container/APIContainerExstensions.cs
@@ -5,6 +5,7 @@ using Quantumart.QP8.BLL.Repository.ArticleMatching.Mappers;
 using Quantumart.QP8.BLL.Repository.ArticleMatching.Models;
 using Quantumart.QP8.BLL.Services.API;
 using QA.Core.ProductCatalog;
+using QA.Core.DPC.QP.Servives;
 
 namespace QA.Core.DPC.API.Container
 {
@@ -13,22 +14,22 @@ namespace QA.Core.DPC.API.Container
 		public static IUnityContainer RegisterArticleMatchService<TCondition, TMapper>(this IUnityContainer container, string connectionString)
 			where TCondition : class
 			where TMapper : IConditionMapper<TCondition>
-		{
-			container.RegisterType<IArticleMatchService<TCondition>, ArticleMatchService<TCondition>>(new InjectionFactory(c => new ArticleMatchService<TCondition>(connectionString, c.Resolve<IConditionMapper<TCondition>>())));
-			container.RegisterType<IConditionMapper<TCondition>, TMapper>();
-			return container;
+		{			
+			return container.RegisterArticleMatchService<TCondition, TMapper>(c => connectionString);
 		}
 
 		public static IUnityContainer RegisterArticleMatchService<TCondition, TMapper>(this IUnityContainer container, Func<IUnityContainer, string> factoryFunc)
 			where TCondition : class
 			where TMapper : IConditionMapper<TCondition>
 		{
-			return container.RegisterArticleMatchService<TCondition, TMapper>(factoryFunc(container));
+            container.RegisterType<IArticleMatchService<TCondition>, ArticleMatchService<TCondition>>(new InjectionFactory(c => new ArticleMatchService<TCondition>(factoryFunc(c), c.Resolve<IConditionMapper<TCondition>>())));
+            container.RegisterType<IConditionMapper<TCondition>, TMapper>();
+            return container;
 		}
 
 		public static IUnityContainer RegisterExpressionArticleMatchService(this IUnityContainer container)
 		{
-			return container.RegisterArticleMatchService<Expression<Predicate<IArticle>>, ExpressionConditionMapper>(c => c.GetConnectionString());
+			return container.RegisterArticleMatchService<Expression<Predicate<IArticle>>, ExpressionConditionMapper>(c => c.Resolve<IConnectionProvider>().GetConnection());
 		}
 	}
 }
--- QA.Core.DPC.QP/Servives/IdentityProvider.cs
+++ QA.Core.DPC.QP/Servives/IdentityProvider.cs
@@ -1,6 +1,7 @@
 ﻿using QA.Core.DPC.QP.Models;
 using System.Security.Principal;
 using System.Threading;
+using System.Web;
 
 namespace QA.Core.DPC.QP.Servives
 {
@@ -10,12 +11,26 @@ namespace QA.Core.DPC.QP.Servives
         {
             get
             {
-                return Thread.CurrentPrincipal.Identity as Identity;
+                var identity = Thread.CurrentPrincipal.Identity as Identity;
+
+                if (identity == null && HttpContext.Current != null)
+                {
+                    identity = HttpContext.Current.User.Identity as Identity;
+                }
+
+                return identity;
             }
 
             set
             {
-                Thread.CurrentPrincipal = new GenericPrincipal(value, new string[0]);
+                var principal = new GenericPrincipal(value, new string[0]);
+
+                if (HttpContext.Current != null)
+                {
+                    HttpContext.Current.User = principal;
+                }
+
+                Thread.CurrentPrincipal = principal;
             }
         }
     }
