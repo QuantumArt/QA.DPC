2e00128742c8527feab8ca53d959159d03144a08
Paul Grigorenko <grigorenko.paul@gmail.com>
2017-10-12 16:25:55 +0300

    добавил контрол SImpleLink и конвертер для StringFormat

---
+++ QA.Core.DPC.UI/Controls/SimpleLink.cs
@@ -0,0 +1,23 @@
+﻿using QA.Core.Models.UI;
+
+namespace QA.Core.DPC.UI.Controls
+{
+    public class SimpleLink : QPControlBase
+    {
+        static SimpleLink()
+        {
+            HrefProperty = DependencyProperty.Register("Href", typeof(string), typeof(SimpleLink));
+        }
+
+        public bool OpenInNewWindow { get; set; } = true;
+        public string IconClass { get; set; } 
+
+        public string Href
+        {
+            get { return (string)GetValue(HrefProperty); }
+            set { SetValue(HrefProperty, value); }
+        }
+
+        public static readonly DependencyProperty HrefProperty;
+    }
+}
