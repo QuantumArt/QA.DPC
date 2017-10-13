e933555bc4f53ef7345450d43bfd5aa4795645c6
Sergey Koryakin <KoryakinS@quantumart.ru>
2017-10-13 14:47:03 +0300

    add custom action for tarantool #109832

---
+++ QA.ProductCatalog.Admin.WebApp/Controllers/TarantoolController.cs
@@ -0,0 +1,74 @@
+﻿using QA.Core.Web;
+using System;
+using System.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace QA.ProductCatalog.Admin.WebApp.Controllers
+{
+    //[RequireCustomAction]
+    public class TarantoolController : Controller
+    {
+        public Uri GetBaseUrl()
+        {
+            var key = ConfigurationManager.AppSettings["Tarantool.SyncApi"];
+            if (!String.IsNullOrEmpty(key))
+            {
+                if (key.StartsWith("/"))
+                {
+                    key = $"{HttpContext.Request.Url?.Scheme}://{HttpContext.Request.Url?.Authority}{key}";
+                }
+
+            }
+            return new Uri(key);
+        }
+
+        [HttpGet]
+        public ActionResult Index(string customerCode)
+        {
+            ViewBag.CustomerCode = customerCode;
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get(string customerCode)
+        {
+            var uri = new Uri(GetBaseUrl(), $"api/Import/{customerCode}/get");
+
+            using (var client = new HttpClient())
+            {
+                return GetJson(await client.GetStringAsync(uri));
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Import(string customerCode)
+        {
+            var uri = new Uri(GetBaseUrl(), $"api/Import/{customerCode}/start");
+            
+            using (var client = new HttpClient())
+            {
+                var result = await client.PostAsync(uri, null);
+                return GetJson(await result.Content.ReadAsStringAsync());
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Stop(string customerCode)
+        {
+            var uri = new Uri(GetBaseUrl(), $"/dpc.tarantool/api/Import/{customerCode}/stop");
+
+            using (var client = new HttpClient())
+            {
+                var result = await client.PostAsync(uri, null);                
+                return GetJson(await result.Content.ReadAsStringAsync());
+            }
+        }
+
+        private ContentResult GetJson(string json)
+        {
+            return Content(json, "application/json");
+        }
+    }
+}
---
+++ QA.ProductCatalog.Admin.WebApp/Scripts/Tarantool.js
@@ -0,0 +1,91 @@
+﻿function ModelData() {
+    this.state = ko.observable('NotRunned');
+    this.progress = ko.observable(0);
+    this.canImport = ko.pureComputed(function () {
+        var state = this.state();
+        return state != 'Running' && state != 'Canceling' && state != 'NotAvailable';
+    }, this);
+    this.canStop = ko.pureComputed(function () {
+        var state = this.state();
+        return state == 'Running';
+    }, this);
+    this.getStateLogo = ko.pureComputed(function () {
+        var state = this.state();
+        if (state == 'Running') {
+            return '../Content/img/TaskStates/Running16.png';
+        }
+        else if (state == 'Finished') {
+            return '../Content/img/TaskStates/Done16.png';
+        }
+        else if (state == 'Error' || state == 'NotAvailable') {
+            return '../Content/img/TaskStates/Failed16.png';
+        }
+        else if (state == 'Canceled' || state == 'Canceling') {
+            return '../Content/img/TaskStates/Cancelled16.png';
+        }
+        else if (state == 'NotRunned') {
+            return '../Content/img/TaskStates/New16.png';
+        }
+        else {
+            return '../Content/img/icons/0.gif';
+        }
+    }, this);
+}
+
+var model = new ModelData();
+
+ko.bindingHandlers.updateProgress = {
+    init: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        $(element).kendoProgressBar({
+            value: viewModel.progress(),
+            type: 'percent'
+        });
+    },
+    update: function (element, valueAccessor, allBindings, viewModel, bindingContext) {
+        $(element).data('kendoProgressBar').value(viewModel.progress());
+    }
+};
+
+$(document).ready(function () {
+    moment.locale('ru');
+    ko.applyBindings(model);
+    updateModel(true);
+
+    $('#import').click(function () {
+        $.post(Url.Content('~/Tarantool/import?customerCode=' + getCustomerCode()))
+            .done(update)
+            .fail(updateWithError);
+    });
+
+    $('#stop').click(function () {
+        $.post(Url.Content('~/Tarantool/stop?customerCode=' + getCustomerCode()))
+            .done(update)
+            .fail(updateWithError);
+    });
+});
+
+function update(json) {
+    model.state(json.state);
+    model.progress(json.progress);
+}
+
+function updateWithError() {
+    model.state('NotAvailable');
+    model.progress(0);
+}
+
+function updateModel(loop) {
+    $.get(Url.Content('~/Tarantool/get?customerCode=' + getCustomerCode()))
+        .done(update)
+        .fail(updateWithError);
+
+    if (loop) {
+        setTimeout(function () {
+            updateModel(loop);
+        }, 2000);
+    }
+}
+
+function getCustomerCode() {
+    return $('#customercode').data('customerCode');
+}
