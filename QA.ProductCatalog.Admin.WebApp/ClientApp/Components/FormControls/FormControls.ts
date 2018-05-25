105d5f47556b64114000a62513c3ef58ba729639
PanyushkinD <PanyushkinD@quantumart.ru>
2018-05-25 17:22:06 +0300

    Att InputSearch

--- QA.ProductCatalog.Admin.WebApp/ClientApp/Components/FormControls/FormControls.ts
+++ QA.ProductCatalog.Admin.WebApp/ClientApp/Components/FormControls/FormControls.ts
@@ -3,6 +3,7 @@ import { InputText } from "./InputText";
 import { InputNumber } from "./InputNumber";
 import { TextArea } from "./TextArea";
 import { DatePicker } from "./DatePicker";
+import { InputSearch } from "./InputSearch";
 import "./FormControls.scss";
 
-export { CheckBox, InputText, InputNumber, TextArea, DatePicker };
+export { CheckBox, InputText, InputNumber, InputSearch, TextArea, DatePicker };
---
+++ QA.ProductCatalog.Admin.WebApp/ClientApp/Components/FormControls/InputSearch.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { action } from "mobx";
+import { observer } from "mobx-react";
+import { AbstractControl } from "./AbstractControls";
+
+@observer
+export class InputSearch extends AbstractControl {
+  handleChange = action((e: any) => {
+    const { model, name } = this.props;
+    model[name] = e.target.value;
+  });
+
+  render() {
+    const { model, name, ...props } = this.props;
+    const inputValue = model[name] != null ? model[name] : "";
+    return (
+      <input
+        type="search"
+        className="form-control form-control-sm"
+        value={inputValue}
+        onChange={this.handleChange}
+        {...props}
+      />
+    );
+  }
+}
