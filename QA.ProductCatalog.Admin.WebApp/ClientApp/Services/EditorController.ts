1450e6829207f9965a4b82932ea842e7d25526c1
PanyushkinD <PanyushkinD@quantumart.ru>
2018-05-31 09:09:37 +0300

    Improve EditorController

--- QA.ProductCatalog.Admin.WebApp/ClientApp/Services/EditorController.ts
+++ QA.ProductCatalog.Admin.WebApp/ClientApp/Services/EditorController.ts
@@ -1,4 +1,5 @@
 import { inject } from "react-ioc";
+import { observable, computed } from "mobx";
 import { DataSerializer } from "Services/DataSerializer";
 import { DataNormalizer } from "Services/DataNormalizer";
 import { DataContext } from "Services/DataContext";
@@ -16,6 +17,14 @@ export class EditorController {
   private _productDefinitionId = Number(this._path.slice(this._rootUrl.length).split("/")[2]);
   private _articleId = Number(this._path.slice(this._rootUrl.length).split("/")[4]);
 
+  @observable public _pendingRequestCount = 0;
+
+  @computed
+  public get hasPendingRequest() {
+    return this._pendingRequestCount > 0;
+  }
+
+  @request
   public async initialize() {
     const initSchemaTask = this.initSchema();
 
@@ -40,6 +49,7 @@ export class EditorController {
     }
   }
 
+  @request
   private async initSchema() {
     const response = await fetch(
       `${this._rootUrl}/ProductEditor/GetProductSchema_Test${this._query}&productDefinitionId=${
@@ -54,4 +64,18 @@ export class EditorController {
     this._dataContext.initSchema(schema.MergedSchemas);
     this._schemaContext.initSchema(schema.EditorSchema);
   }
+}
+
+function request(_target: EditorController, _key: string, descriptor: PropertyDescriptor) {
+  return {
+    ...descriptor,
+    async value(this: EditorController, ...args: any[]) {
+      try {
+        this._pendingRequestCount++;
+        return await descriptor.value.apply(this, args);
+      } finally {
+        this._pendingRequestCount--;
+      }
+    }
+  };
 }
