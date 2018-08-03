a99612ceebbf90572e381ec1e32cf1139faf3e29
PanyushkinD <PanyushkinD@quantumart.ru>
2018-08-03 16:05:41 +0300

    Simplify DataSerializer

--- QA.ProductCatalog.Admin.WebApp/ClientApp/Services/DataSerializer.ts
+++ QA.ProductCatalog.Admin.WebApp/ClientApp/Services/DataSerializer.ts
@@ -5,29 +5,25 @@ import { ArticleObject, isArticleObject } from "Models/EditorDataModels";
  * Отображения положительных серверных Id на  отрицательные клиентские,
  * сгруппированные по имени контента.
  */
-export interface IdMappingsByContent {
-  [contentName: string]: {
-    [serverArticleId: number]: number;
-  };
+export interface IdMapping {
+  ClientId: number;
+  ServerId: number;
 }
 
 export class DataSerializer {
-  private _idMappingsByContent: IdMappingsByContent = Object.create(null);
+  private _idMappingDict: { [serverId: number]: number } = Object.create(null);
 
   /** Обновление соответствия положительные серверные Id => отрицательные клиентские Id  */
-  public extendIdMappings(idMappingsByContent: IdMappingsByContent) {
-    Object.entries(idMappingsByContent).forEach(([contentName, mappingDict]) => {
-      let currentDict = this._idMappingsByContent[contentName];
-      if (!currentDict) {
-        this._idMappingsByContent[contentName] = currentDict = Object.create(null);
-      }
-      Object.entries(mappingDict).forEach(([serverId, clientId]: any) => {
-        currentDict[serverId] = clientId;
-      });
+  public extendIdMappings(idMappings: IdMapping[]) {
+    idMappings.forEach(pair => {
+      this._idMappingDict[pair.ServerId] = pair.ClientId;
     });
   }
 
-  /** Заменяет все отрицательные клиентские Id на соответствующие им серверные Id (если они уже определены). */
+  /**
+   * Заменяет все отрицательные клиентские Id на соответствующие им
+   * серверные Id (если они уже определены).
+   */
   public serialize(article: ArticleObject): string {
     function toObject(argument: any) {
       if (isObject(argument)) {
@@ -49,21 +45,21 @@ export class DataSerializer {
     return JSON.stringify(toObject(article));
   }
 
-  /** Заменяет все серверные Id на соответствующие им клиентские отрицательные Id (если они определены) */
+  /**
+   * Заменяет все серверные Id на соответствующие им
+   * клиентские отрицательные Id (если они определены).
+   * Преобразует ISO Date strings в Unix time.
+   */
   public deserialize<T = any>(json: string): T {
     return JSON.parse(json, (_key, value) => {
       if (isArticleObject(value)) {
-        const clientId = this.getClientId(value);
-        return { ...value, _ClientId: clientId };
+        // @ts-ignore
+        value._ClientId = this._idMappingDict[value._ServerId] || value._ServerId;
+        return value;
       } else if (isIsoDateString(value)) {
         return Number(new Date(value));
       }
       return value;
     });
-  }
-
-  private getClientId(article: ArticleObject) {
-    const mappingDict = this._idMappingsByContent[article._ContentName];
-    return (mappingDict && mappingDict[article._ServerId]) || article._ServerId;
   }
 }
