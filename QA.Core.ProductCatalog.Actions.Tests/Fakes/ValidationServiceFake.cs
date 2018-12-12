849f0b30033cdc6871cf7c6a26ba144ce5cdbbc2
Sergey Koryakin <KoryakinS@quantumart.ru>
2018-12-12 17:56:26 +0300

    #131068 update tests

--- QA.Core.ProductCatalog.Actions.Tests/Fakes/ValidationServiceFake.cs
+++ QA.Core.ProductCatalog.Actions.Tests/Fakes/ValidationServiceFake.cs
@@ -1,11 +1,21 @@
 ﻿using QA.ProductCatalog.Infrastructure;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace QA.Core.ProductCatalog.Actions.Tests.Fakes
 {
     public class ValidationServiceFake : IValidationService
     {
+        public int[] GetProductIds()
+        {
+            return new int[0];
+        }
+
         public void UpdateValidationInfo(int[] productIds, ConcurrentDictionary<int, string> errors)
+        {
+        }
+
+        public void ValidateAndUpdate(int[] productIds, Dictionary<int, string> errors)
         {
         }
     }
