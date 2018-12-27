43c7ddc3702adc2516903a9073fff74e1f4baa29
Sergey Koryakin <KoryakinS@quantumart.ru>
2018-12-27 20:42:37 +0300

    #131754 update web api unity config

--- QA.ProductCatalog.WebApi/App_Start/UnityConfig.cs
+++ QA.ProductCatalog.WebApi/App_Start/UnityConfig.cs
@@ -83,12 +83,19 @@ namespace QA.ProductCatalog.WebApi.App_Start
             if (connection.QPMode)
             {
                 unityContainer.RegisterConsolidationCache(autoRegister).As<IFactory>().With<FactoryWatcher>(watcherInterval).Watch();
-                unityContainer.RegisterQpMonitoring();
             }
             else
             {
                 unityContainer.RegisterType<ICustomerProvider, SingleCustomerProvider>();
                 unityContainer.RegisterConsolidationCache(autoRegister, SingleCustomerProvider.Key).As<IFactory>().With<FactoryWatcher>().Watch();
+            }
+
+            if (connection.QPMode || connection.UseQPMonitoring)
+            {
+                unityContainer.RegisterQpMonitoring();
+            }
+            else
+            {
                 unityContainer.RegisterNonQpMonitoring();
             }
 
