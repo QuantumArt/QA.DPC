543f6ccec198f3a72211b16330b375e235c23ce5
Pavel Celut <CelutP@gmail.com>
2020-02-21 15:11:21 +0300

    Add logging

--- QA.Core.ProductCatalog.TaskScheduler/TaskJob.cs
+++ QA.Core.ProductCatalog.TaskScheduler/TaskJob.cs
@@ -1,6 +1,8 @@
-﻿using QA.Core.ProductCatalog.ActionsRunner;
-using QA.Core.ProductCatalog.ActionsRunnerModel;
+﻿using NLog;
+using NLog.Fluent;
+using QA.Core.ProductCatalog.ActionsRunner;
 using Quartz;
+using Unity.Injection;
 using Task = System.Threading.Tasks.Task;
 
 namespace QA.Core.ProductCatalog.TaskScheduler
@@ -8,6 +10,10 @@ namespace QA.Core.ProductCatalog.TaskScheduler
     public class TaskJob : IJob
     {
 	    private readonly ITaskService _service;
+
+	    private static ILogger _logger = LogManager.GetCurrentClassLogger();
+	    
+	    
 		public TaskJob(ITaskService service)
 		{
 			_service = service;
@@ -17,6 +23,13 @@ namespace QA.Core.ProductCatalog.TaskScheduler
 		{
 			var dataMap = context.JobDetail.JobDataMap;
 			var id = dataMap.GetIntValue("SourceTaskId");
+			
+			_logger.Info()
+				.Message("Calling task {taskId} by schedule.", id)
+				.Property("scheduleTime", context.ScheduledFireTimeUtc)
+				.Property("fireTime", context.FireTimeUtc)
+				.Write();
+
 			_service.SpawnTask(id);
 			return Task.CompletedTask;
 		}
