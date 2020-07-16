8f99063daffd7fddb89986a97cea1425dafb6f0f
MikulenyaI <mikulenyai@quantumart.ru>
2020-07-16 11:49:55 +0300

    #157319 Стилизация для HighloadFront

--- QA.Core.DPC.Resources/HfStrings.Designer.cs
+++ QA.Core.DPC.Resources/HfStrings.Designer.cs
@@ -19,7 +19,7 @@ namespace QA.Core.DPC.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class HfStrings {
@@ -111,6 +111,15 @@ namespace QA.Core.DPC.Resources {
         public static string Processing {
             get {
                 return ResourceManager.GetString("Processing", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Progress.
+        /// </summary>
+        public static string Progress {
+            get {
+                return ResourceManager.GetString("Progress", resourceCulture);
             }
         }
         
