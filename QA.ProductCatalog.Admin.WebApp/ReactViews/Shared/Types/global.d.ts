f60a6e7ff6ae24e84601f4bbd146357bd46c49da
Vladimir_K <kupriyanov.vo@gmail.com>
2020-07-17 14:48:28 +0300

    add global window types

--- QA.ProductCatalog.Admin.WebApp/ReactViews/Shared/Types/global.d.ts
+++ QA.ProductCatalog.Admin.WebApp/ReactViews/Shared/Types/global.d.ts
@@ -1,1 +1,32 @@
-﻿declare interface Window {}
+﻿/**
+ * Global definitions for developement
+ * DO NOT IMPORT ANYTHING OR YOU WILL BREAK BUILD PROCESS!
+ */
+
+declare interface Window {
+  QP: {
+    //for Task view
+    Tasks: {
+      tableFields: {
+        userName: string;
+        status: number;
+        statusValues: string[];
+        schedule: boolean;
+        progress: number;
+        name: string;
+        created: string;
+        lastStatusChange: string;
+        message: string;
+      };
+      tableFilters: {
+        //отображение названий в селекте фильтров
+        messages: {
+          isFalse: string;
+          isTrue: string;
+          filter: string;
+          clear: string;
+        };
+      };
+    };
+  };
+}
