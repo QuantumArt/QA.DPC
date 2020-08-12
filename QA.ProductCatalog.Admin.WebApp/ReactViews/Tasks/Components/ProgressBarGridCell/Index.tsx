bce841c5a18e27340982c8bce1d2c2d93b641b44
Vladimir_K <kupriyanov.vo@gmail.com>
2020-08-12 14:17:52 +0300

    fix for progress bar label

--- QA.ProductCatalog.Admin.WebApp/ReactViews/Tasks/Components/ProgressBarGridCell/Index.tsx
+++ QA.ProductCatalog.Admin.WebApp/ReactViews/Tasks/Components/ProgressBarGridCell/Index.tsx
@@ -33,7 +33,6 @@ export const ProgressBarGridCell = ({ value, stateId }: { value: number; stateId
     <div className="progress-bar-cell">
       <ProgressBar
         defaultBarProps={progressBarProps}
-        withLabel={false}
         barWidth="140px"
         labelWidth="40px"
         labelClassName="progress-bar-cell__label"
