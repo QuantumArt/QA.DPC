00c4ac83603a0085966a82d839b72fe0f56117a4
Pavel Celut <CelutP@gmail.com>
2021-01-11 14:12:17 +0300

    Add bat file

---
+++ ImpactService/Install/Install.bat
@@ -0,0 +1,12 @@
+
+@ECHO OFF
+
+SET /P ElasticsearchHost= Elasticsearch:
+
+if not exist "C:\QA" mkdir C:\QA
+if not exist "C:\QA\Logs" mkdir C:\QA\Logs
+
+SET ThisScriptsDirectory=%~dp0
+SET PowerShellScriptPath=%ThisScriptsDirectory%\Install\InstallImpact.ps1
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File """"%PowerShellScriptPath%"""" -port 8033 -logPath """"C:\QA\Logs"""" -elasticBaseAddress """"%ElasticsearchHost%"""" -liveIndexName products -stageIndexName products_stage' -Verb RunAs}";
--- install/install_pg.bat
+++ install/install_pg.bat
@@ -13,5 +13,4 @@ if not exist "C:\QA\Logs" mkdir C:\QA\Logs
 SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath=%ThisScriptsDirectory%\Install\InstallConsolidationCatalog.ps1
 
-for /r %%a in (*.ps1) do (echo.>%%a:Zone.Identifier)
 PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File """"%PowerShellScriptPath%"""" -dbType 1  -databaseServer """"%DatabaseServer%"""" -login """"%DatabaseLogin%"""" -password """"%DatabasePassword%""""  -customerCode """"%CustomerCode%"""" -installRoot """"C:\QA"""" -elasticsearchHost """"%ElasticsearchHost%"""" -backendPort """"%BackendPort%"""" -logPath """"C:\QA\Logs"""" ' -Verb RunAs}";
