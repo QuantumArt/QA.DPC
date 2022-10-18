ab18c8cf10767f9b8a69173e4bfac1d3728c03c9
Pavel Celut <CelutP@gmail.com>
2022-10-18 22:11:35 +0300

    Split into 2 builds

--- move/move-dpc-front-image.sh
+++ move/move-dpc-front-image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker pull '||registry-source||/dpc-front:||version||' && \
-docker tag '||registry-source||/dpc-front:||version||' '||registry-destination||/dpc-front:||version||' && \
-docker push '||registry-destination||/dpc-front:||version||'
+docker tag '||registry-source||/dpc-front:||version||' '||registry||/dpc-front:||version||' && \
+docker push '||registry||/dpc-front:||version||'
--- move/move-dpc-impact-image.sh
+++ move/move-dpc-impact-image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker pull '||registry-source||/dpc-impact:||version||' && \
-docker tag '||registry-source||/dpc-impact:||version||' '||registry-destination||/dpc-impact:||version||' && \
-docker push '||registry-destination||/dpc-impact:||version||'
+docker tag '||registry-source||/dpc-impact:||version||' '||registry||/dpc-impact:||version||' && \
+docker push '||registry||/dpc-impact:||version||'
