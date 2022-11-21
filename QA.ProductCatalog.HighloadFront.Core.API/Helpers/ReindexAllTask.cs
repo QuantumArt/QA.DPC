db89affcae0523a0ec6bb521e59ae4cef5344044
Evgeniy Timofeev <TimofeevE@quantumart.ru>
2022-11-21 17:45:08 +0300

    Fix bug with index delete after creation

--- QA.ProductCatalog.HighloadFront.Core.API/Helpers/ReindexAllTask.cs
+++ QA.ProductCatalog.HighloadFront.Core.API/Helpers/ReindexAllTask.cs
@@ -38,6 +38,7 @@ namespace QA.ProductCatalog.HighloadFront.Core.API.Helpers
                 {
                     if (_importer.ValidateInstance(language, state))
                     {
+                        var indexesToDelete = _manager.GetIndexesAsync(language, state, _stores).Result;
                         var newIndex = _manager.CreateVersionedIndexAsync(language, state, _stores).Result;
 
                         if (string.IsNullOrWhiteSpace(newIndex))
@@ -49,7 +50,6 @@ namespace QA.ProductCatalog.HighloadFront.Core.API.Helpers
                         _importer.ImportAsync(executionContext, language, state, _stores, newIndex).Wait();
 
                         string alias = _configuration.GetElasticIndex(language, state).Name;
-                        var indexesToDelete = _manager.GetIndexesAsync(language, state, _stores).Result;
 
                         if (indexesToDelete.Contains(alias))
                         {
