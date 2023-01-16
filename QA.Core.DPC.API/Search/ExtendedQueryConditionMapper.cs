1a7982b0f8dfca720f83f4ab4ecdd82bdd62eb3f
Dakhraui Mokhamed <dakhrauim@qantumart.ru>
2023-01-16 16:07:11 +0300

    Support boolean fields in product search
    #170837

--- QA.Core.DPC.API/Search/ExtendedQueryConditionMapper.cs
+++ QA.Core.DPC.API/Search/ExtendedQueryConditionMapper.cs
@@ -53,7 +53,10 @@ namespace QA.Core.DPC.API.Search
                             .ToArray()
                     };
                 }                
-                else if (property.Value.Type == JTokenType.String || property.Value.Type == JTokenType.Integer || property.Value.Type == JTokenType.Float)
+                else if (property.Value.Type == JTokenType.String
+                    || property.Value.Type == JTokenType.Integer
+                    || property.Value.Type == JTokenType.Float
+                    || property.Value.Type == JTokenType.Boolean)
                 {
                     return MapFields(source.GetQuery(property));
                 }              
@@ -199,6 +202,21 @@ namespace QA.Core.DPC.API.Search
                         else
                         {
                             throw new Exception("field " + field.Name + " must be date");
+                        }
+                    }
+                    else if (field.ExactType == FieldExactTypes.Boolean)
+                    {
+                        if (property.Value.Type == JTokenType.Boolean)
+                        {
+                            value = property.Value.Value<bool>();
+                        }
+                        else if (bool.TryParse(stringValue, out var boolValue))
+                        {
+                            value = boolValue;
+                        }
+                        else
+                        {
+                            throw new Exception("field " + field.Name + " must be boolean");
                         }
                     }
                 }
