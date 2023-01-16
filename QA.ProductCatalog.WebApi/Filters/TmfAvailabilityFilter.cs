f74ea000c41647901c4737e722e91a018d41ac77
Dakhraui Mokhamed <dakhrauim@qantumart.ru>
2023-01-16 16:13:28 +0300

    Remove unused method

--- QA.ProductCatalog.WebApi/Filters/TmfAvailabilityFilter.cs
+++ QA.ProductCatalog.WebApi/Filters/TmfAvailabilityFilter.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc.Controllers;
-using Microsoft.CodeAnalysis;
-using Microsoft.OpenApi.Models;
+﻿using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Linq;
 
@@ -27,28 +25,6 @@ namespace QA.ProductCatalog.WebApi.Filters
                     _ = swaggerDoc.Paths.Remove(route);
                 }
             }
-        }
-
-        private static bool HasCustomAttribute<TAttribute>(ControllerActionDescriptor controllerActionDescriptor)
-        {
-            if (controllerActionDescriptor is null)
-            {
-                return false;
-            }
-
-            // Check if the attribute exists on the action method
-            if (controllerActionDescriptor.MethodInfo?.GetCustomAttributes(true)?.Any(a => a.GetType().Equals(typeof(TAttribute))) ?? false)
-            {
-                return true;
-            }
-
-            // Check if the attribute exists on the controller
-            if (controllerActionDescriptor.ControllerTypeInfo?.GetCustomAttributes(typeof(TAttribute), true)?.Any() ?? false)
-            {
-                return true;
-            }
-
-            return false;
         }
     }
 }
