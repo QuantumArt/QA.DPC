da3a7749ec57cd576652cf6415d05102640d8b28
Evgeniy Timofeev <TimofeevE@quantumart.ru>
2023-03-09 11:57:26 +0300

    Api disable for tmf and non tmf mode

---
+++ QA.ProductCatalog.TmForum/Filters/DisableDefaultApiAttribute.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using QA.ProductCatalog.ContentProviders;
+
+namespace QA.ProductCatalog.TmForum.Filters;
+
+public class DisableDefaultApiAttribute : TypeFilterAttribute
+{
+    public DisableDefaultApiAttribute() : base(typeof(DisableDefaultApiFilter))
+    {
+
+    }
+    
+    private class DisableDefaultApiFilter : IAuthorizationFilter
+    {
+        private readonly ISettingsService _settingsService;
+
+        public DisableDefaultApiFilter(ISettingsService settingsService)
+        {
+            _settingsService = settingsService;
+        }
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            if (bool.TryParse(_settingsService.GetSetting(SettingsTitles.TMF_ENABLED), out bool tmfEnabled) &&
+                tmfEnabled)
+            {
+                context.Result = new NotFoundResult();
+            }
+        }
+    }
+}
