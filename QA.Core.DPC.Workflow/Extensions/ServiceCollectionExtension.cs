524d0df88ecec46f7256ff7f187cc9eb4cc610c6
Evgeniy Timofeev <TimofeevE@quantumart.ru>
2023-03-28 17:40:47 +0300

    Add tenant watcher

--- QA.Core.DPC.Workflow/Extensions/ServiceCollectionExtension.cs
+++ QA.Core.DPC.Workflow/Extensions/ServiceCollectionExtension.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using QA.Core.DPC.Workflow.ExternalTasks;
+using QA.Core.DPC.Workflow.Models;
+using QA.Core.DPC.Workflow.Services;
 using QA.Workflow.TaskWorker.Extensions;
 using QA.Workflow.TaskWorker.Interfaces;
 using QA.Workflow.TaskWorker.Models;
@@ -11,6 +14,18 @@ public static class ServiceCollectionExtension
 {
     public static IServiceCollection RegisterWorkflow(this IServiceCollection services, IConfiguration configuration)
     {
+        ExtendedCamundaSettings settings = new();
+        configuration.GetSection("Camunda").Bind(settings);
+
+        if (!settings.IsEnabled)
+        {
+            return services;
+        }
+
+        services.AddSingleton(Options.Create(settings));
+
+        services.AddHostedService<WorkflowTenantWatcherHostedService>();
+
         // Add camunda worker
         IExternalTaskCollection taskCollection = services.RegisterCamundaExternalTaskWorker(configuration);
         // Register publish task
@@ -23,7 +38,6 @@ public static class ServiceCollectionExtension
         // Add tenants to listen in camunda
         // ToDo: move to it's actual location before merge
         WorkflowTenants tenants = new();
-        tenants.Tenants.Add("qmobile_catalog");
         services.AddSingleton(tenants);
 
         return services;
---
+++ QA.Core.DPC.Workflow/Services/WorkflowTenantWatcherHostedService.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using QA.Core.DPC.QP.Models;
+using QA.Core.DPC.QP.Services;
+using QA.Core.DPC.Workflow.Models;
+using QA.ProductCatalog.ContentProviders;
+using QA.Workflow.TaskWorker;
+using QA.Workflow.TaskWorker.Models;
+
+namespace QA.Core.DPC.Workflow.Services
+{
+    public class WorkflowTenantWatcherHostedService : IHostedService
+    {
+        private readonly CancellationTokenSource _cancellationToken;
+        private readonly ILogger<ExternalTaskHostedService> _logger;
+        private readonly ICustomerProvider _customerProvider;
+        private readonly ExtendedCamundaSettings _camundaSettings;
+        private readonly WorkflowTenants _workflowTenants;
+        private readonly IIdentityProvider _identityProvider;
+        private readonly IServiceProvider _serviceProvider;
+        private Task _executionTask;
+
+        public WorkflowTenantWatcherHostedService(ILogger<ExternalTaskHostedService> logger,
+            ICustomerProvider customerProvider,
+            IOptions<ExtendedCamundaSettings> camundaSettings,
+            WorkflowTenants workflowTenants,
+            IIdentityProvider identityProvider,
+            IServiceProvider serviceProvider)
+        {
+            _logger = logger;
+            _cancellationToken = new();
+            _customerProvider = customerProvider;
+            _camundaSettings = camundaSettings.Value;
+            _workflowTenants = workflowTenants;
+            _identityProvider = identityProvider;
+            _serviceProvider = serviceProvider;
+        }
+
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _executionTask = Watch(_cancellationToken.Token);
+
+            _logger.LogInformation("Workflow external task worker started.");
+            return _executionTask.IsCompleted ? _executionTask : Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (_executionTask == null)
+            {
+                return;
+            }
+
+            try
+            {
+                _cancellationToken.Cancel();
+            }
+            finally
+            {
+                await Task.WhenAny(_executionTask, Task.Delay(Timeout.Infinite, cancellationToken));
+            }
+        }
+
+        private async Task Watch(CancellationToken cancellationToken)
+        {
+            // Wait some time for customer_codes registration
+            await Task.Delay(TimeSpan.FromMinutes(1), cancellationToken);
+
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    Customer[]? customers = _customerProvider.GetCustomers();
+
+                    foreach (Customer customer in customers)
+                    {
+                        try
+                        {
+                            _identityProvider.Identity = new(customer.CustomerCode);
+                            ISettingsService settingsService = _serviceProvider.GetRequiredService<ISettingsService>();
+                            string? camundaSetting = settingsService.GetSetting(SettingsTitles.EXTERNAL_WORKFLOW);
+
+                            if (string.IsNullOrWhiteSpace(camundaSetting))
+                            {
+                                ResolveTenant(customer.CustomerCode, TenantAction.Remove);
+                                continue;
+                            }
+
+                            if (!bool.TryParse(camundaSetting, out bool camundaEnabled) || !camundaEnabled)
+                            {
+                                ResolveTenant(customer.CustomerCode, TenantAction.Remove);
+                                continue;
+                            }
+
+                            ResolveTenant(customer.CustomerCode, TenantAction.Add);
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogError(ex, "Error while reading settings for tenant {tenant}", customer.CustomerCode);
+                        }
+                        finally
+                        {
+                            _identityProvider.Identity = null;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error while loading tenants for workflow from config.");
+                }
+                finally
+                {
+                    _identityProvider.Identity = null;
+                    await Task.Delay(_camundaSettings.TenantWatcherWaitTime, cancellationToken);
+                }
+            }
+        }
+
+        private void ResolveTenant(string tenant, TenantAction action)
+        {
+            switch (action)
+            {
+                case TenantAction.Add:
+                    if (!_workflowTenants.Tenants.Contains(tenant))
+                    {
+                        _workflowTenants.Tenants.Add(tenant);
+                        _logger.LogInformation("Customer code {code} was added to workflow tenant list.", tenant);
+                    }
+                    break;
+                case TenantAction.Remove:
+                    if (_workflowTenants.Tenants.Contains(tenant))
+                    {
+                        _workflowTenants.Tenants.Remove(tenant);
+                        _logger.LogInformation("Customer code {code} was removed from workflow tenant list.", tenant);
+                    }
+                    break;
+                default:
+                    throw new InvalidOperationException($"Unsupported action {action} for tenant {tenant}");
+            }
+        }
+    }
+}
