45d9949f250626d00d92d9de693df4c096410987
Evgeniy Timofeev <TimofeevE@quantumart.ru>
2023-04-19 13:03:36 +0300

    Add move script & rename docker file
    Add move script for kafka-api
    Rename dockerfile for kafka-api

---
+++ move/move-dpc-kafka-api-image.sh
@@ -0,0 +1,4 @@
+﻿#!/bin/sh
+docker pull '||registry-source||/dpc-kafka-api:||version||' && \
+docker tag '||registry-source||/dpc-kafka-api:||version||' '||registry||/dpc-kafka-api:||version||' && \
+docker push '||registry||/dpc-kafka-api:||version||'
