3366d62fb7df5f663b6bf5565912575da9bf2078
Sergey Koryakin <KoryakinS@quantumart.ru>
2023-12-19 16:35:16 +0300

    #173569 add NumberFormatConverter

---
+++ QA.Core.DPC.UI/Converters/NumberFormatConverter.cs
@@ -0,0 +1,36 @@
+﻿using QA.Core.Models;
+using QA.Core.Models.UI;
+using System;
+using System.Globalization;
+
+namespace QA.Core.DPC.UI.Converters
+{
+    public class NumberFormatConverter : DependencyObject, IValueConverter
+    {
+        public static readonly DependencyProperty FormatProperty;
+
+        static NumberFormatConverter()
+        {
+            FormatProperty = DependencyProperty.Register("Format", typeof(string), typeof(NumberFormatConverter));
+        }
+
+        public string Format
+        {
+            get { return GetValue<string>(FormatProperty); }
+            set { SetValue(FormatProperty, value); }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType == typeof(string) && !string.IsNullOrWhiteSpace(Format) && value is string stringValue)
+            {
+                if (decimal.TryParse(stringValue, out decimal number))
+                {
+                    return number.ToString(Format, culture);
+                }
+            }
+
+            return value;
+        }
+    }
+}
