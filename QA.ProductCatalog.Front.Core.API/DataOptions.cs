b8dcc1a91b48ae938259b53061c136c531e04dba
CelutP <CelutP@gmail.com>
2024-10-24 18:10:22 +0300

    Fix nuget + add timeout

--- QA.ProductCatalog.Front.Core.API/DataOptions.cs
+++ QA.ProductCatalog.Front.Core.API/DataOptions.cs
@@ -2,6 +2,8 @@
 {
     public class DataOptions
     {
+        public int CommandTimeout { get; set; } = 60;
+        
         public string FixedConnectionString { get; set; }
         
         public string FixedCustomerCode { get; set; }
